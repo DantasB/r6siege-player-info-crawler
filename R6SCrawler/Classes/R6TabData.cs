ec5ca2a92b9ca33c2f44b379ab1309577cdf69c6
Bruno Dantas de Paiva <bruno.d.paiva@hotmail.com>
2020-04-05 01:01:11 -0300

    Add files via upload

---
+++ R6SCrawler/Classes/R6TabData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R6SCrawler
+{
+    class R6TabData
+    {
+        public Dictionary<string, string> Stats { get; set; }
+
+    }
+}
